9130bee681f82660d942b60e03fd50c540ae0fcf
AndrewJacop <73601700+AndrewJacop@users.noreply.github.com>
2022-11-13 00:34:33 +0200

    added the TODO list until Challenge B

---
+++ lib/question.dart
@@ -0,0 +1,3 @@
+//TODO 1: create Question CLass with 2 proprieties: a string "questionText", a bool "questionAnswer"
+//TODO 2: SELF STUDY: search "Class and Constructors in Dart"
+//TODO 3: make a Question Constructor (not named or optional)
